f8ff34ca950e52cd3b5cab2766036bfad0a2585f
Robert Nuorteva <roba@hey.com>
2022-02-24 16:25:08 +0200

    Task 13.6.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,20 +7,30 @@ router.get('/', async (req, res) => {
   res.json(blogs)
  })
  
- router.post('/', async (req, res) => {
-   try {
-     const blog = await Blog.create(req.body)
-     return res.json(blog)
-   } catch (error) {
-     return res.status(400).json({ error })
-   }
- })
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
  
- router.delete('/:id', blogFinder, async (req, res) => {
-   if (req.blog) {
-     await req.blog.destroy()
-   }
-   res.status(204).end()
- })
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
+  }
+  res.status(204).end()
+})
 
  module.exports = router
