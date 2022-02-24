ff4e4d5e7bc2b46b78d0573e8ed4f4a18439feb8
Robert Nuorteva <roba@hey.com>
2022-02-24 15:08:38 +0200

    Task 13.4.

--- commands.sql
+++ commands.sql
@@ -1,5 +1,6 @@
 CREATE TABLE blogs (id SERIAL PRIMARY KEY, author text, url text NOT NULL, title text NOT NULL, likes integer DEFAULT 0); 
 
 insert into blogs (author, url, title) values ('Will Writer', 'someurl.com/newblog', 'On humility');
+insert into blogs (author, url, title, likes) values ('Wendy Writer', 'someurl.com/newblog', 'On courage', 49);
 
-select * from blogs;
+SELECT * FROM blogs;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+app.use(express.json())
+const { Sequelize, Model, DataTypes } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -10,16 +13,67 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    const blogs = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT })
-    blogs.forEach(blog => console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`))
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
+class Blog extends Model {}
 
-main()
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync() // if table blogs doesn't exist, create the table
+
+app.get('/api/blogs', async (req, res) => {
+ const blogs = await Blog.findAll()
+ res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  console.log('logging here:', req.params.id)
+  try {
+    await Blog.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    return res.status(204).end()
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
